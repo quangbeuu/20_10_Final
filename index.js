a9403c5ae3bffb4bb7d026af7ada180fda074394
Quangvu9501 <77784866+Quangvu9501@users.noreply.github.com>
2021-10-20 00:21:21 +0700

    Project

---
+++ index.js
@@ -0,0 +1,199 @@
+let girl = [
+    {
+        surname: 'Cô Cao Thu',
+        name: 'Hương',
+        img: '../img/CoHuong.jpg',
+        desc: 'Nhân ngày Phụ Nữ Việt Nam 20/10, chúng em chúc cô có thật nhiều niềm vui và thành công về mọi mặt trong cuộc sống.Hi vọng cô sẽ cùng chúng em có những trải nghiệm thật tốt đẹp tại ngôi trường mơ ước này. Một lần nữa cho phép em gửi lời cảm ơn chân thành tới cô vì đã giúp đỡ và dìu dắt chúng em suốt thời gian vừa qua!'
+    },
+    {
+        surname: 'Hà Phạm',
+        name: 'Lan Anh',
+        img: '../img/La.jpg',
+        desc:'Ôi b ơi. 20/10 zui zẻ , học giỏi. Cơ mà nếu mà b muốn giảm cân thì chúc b hoàn thành mong muốn. Còn ko thì thoai nha b :))'
+    },
+    {
+        surname: 'Nguyễn',
+        name: 'Mai Anh',
+        img: '../img/MaiAnh.jpg',
+        desc:'Bạn MA ngày càng học giỏi nha. Chúc bạn ra trường sớm. Sau bạn có giàu thì làm ny t nhá :))'
+    },
+    {
+        surname: 'Nguyễn',
+        name: 'Phương Anh',
+        img: '../img/PhuongAn.jpg',
+        desc:''
+    },
+    {
+        surname: 'Nguyễn',
+        name: 'Quỳnh Anh',
+        img: '../img/Qa.png',
+        desc:'Chúc Cocoa ngày càng thành công, hạnh phúc và gặp nhiều may mắn nha '
+    },
+    {
+        surname: 'Phạm Thị',
+        name: 'Phương Anh',
+        img: '../img/Pa.jpg',
+        desc:'Chúc Phương Anh 20/10 vui vẻ, ăn hoài hông bếu'
+    },
+    {
+        surname: 'Hà Mỹ',
+        name: 'Dung',
+        img: '../img/MyDung.jpg',
+        desc:'Zê. Chúc bạn ngày nào cũng vui vẻ. Sớm tạo đc cái app ghép đôi cho t dùng nhá :))'
+    },
+    {
+        surname: 'Mai Thị',
+        name: 'Ánh Dương',
+        img: '../img/AnhDuong.jpg',
+        desc:'Chúc Ánh Dương 20/10 gặp nhiều may mắn, hạnh phúc hơn, nhiều niềm vui, nhiều điều tuyệt vời hơn trong cuộc sống. Always be Shining.'
+    },
+    {
+        surname: 'Mai Thị',
+        name: 'Gấm',
+        img: '../img/MaiGam.jpg',
+        desc:'Chúc bạn Gấm của tôi 20/10 luôn vui vẻ, nhận được nhiều lời chúc cũng như thật nhiều quà từ cánh mày râu nhé. Học bổng thì chắc chắn có rồi nên sau này có giàu thì cho t xin chân tài xế riêng nhaaaa!!!!!!!! Đố biết t là ai <3 '
+    },
+    {
+        surname: 'Trần Thị',
+        name: 'Hạnh',
+        img: '../img/TranHanh.jpg',
+        desc:'20-10 luôn thành công rực rỡ, được nhiều bé theo đuổi và giành được nhiều học bổng và huân chương vàng nha <3'
+    },
+    {
+        surname: 'Đồng Thị Thu',
+        name: 'Hằng',
+        img: '../img/DongHang.jpg',
+        desc:'Chúc cậu 20-10 thật vui vẻ, học giỏi, sớm có ny nha (nếu có rồi thì có thêm cũng chẳng sao phụ nữ luôn được ưu tiên mà :))!!!)'
+    },
+    {
+        surname: 'Nguyễn Thị',
+        name: 'Huyền',
+        img: '../img/huyen.jpg',
+        desc:'Chúc bạn có một cuộc đời luôn tươi đẹp, nhiều ng yêu, hòa đồng các bạn trong lớp hơn <3'
+    },
+    {
+        surname: 'Lâm Thị',
+        name: 'Hương',
+        img: '../img/LamHuong.jpg',
+        desc:'20/10 vui vẻ nha bạn ơi, hãy enjoy cái monment này và keep cơ thể clear nha =))))'
+    },
+    {
+        surname: 'Trần Thị',
+        name: 'Hương',
+        img: '../img/HuongBaVi.jpg',
+        desc:'Zui zẻ nha mầy. Bớt nói ngọng vơi hỏi bài đi nha :))'
+    },
+    {
+        surname: 'Trần Thị',
+        name: 'Hương',
+        img: '../img/huongkobavi.jpg',
+        desc:'Chúc Hương 20-10 vui vẻ và hạnh phúc. Học gỏi và cố gắng giành được học bổng'
+    },
+    {
+        surname: 'Nguyễn Thị',
+        name: 'Khánh Linh',
+        img: '../img/LinhNguyen.jpg',
+        desc:'Chúc Linh luôn xinh đẹp, sang Đức có nhiều bạn bè hơn, cởi mở, đừng độc thân vui tính nữa, chúc bạn sẽ tìm được thành công nghệ truyền thông'
+    },
+    {
+        surname: 'Nguyễn Thị',
+        name: 'Thanh Nhàn',
+        img: '../img/ThanhNhan.jpg',
+        desc:'Đọc được cái này Nhàn sẽ biết tớ là ai, bởi tớ luôn nhớ cảnh cậu nhẹ nhàng nhét chiếc bánh kếp (to) vào miệng. Nhân ngày phụ nữ Việt Nam 20-10, tớ chúc Nhàn luôn xinh đẹp, khỏe mạnh, giàu có và dịu dàng (với tớ). Mong lúc đi học off, cậu lại ăn bánh kếp cho tớ xem (tớ bao) ^^'
+    },
+    {
+        surname: 'Nguyễn Thị',
+        name: 'Nhung',
+        img: '../img/111.png',
+        desc:'Muốn chúc bạn chăm chỉ và học giỏi hay đạt học bổng. Nhưng mà bạn có thiếu đâu mà tôi chúc. Thôi thì ôi bạn ơi, đi kiếm ny đc ròi b ợ'
+    },
+    {
+        surname: 'Bùi Thị Dung',
+        name: 'Nhi',
+        img: '../img/NhiBui.jpg',
+        desc:'Chúc Nhi Bùi ngày 20/10 zui zẻ. Sớm có người yêu và kì nào cũng có học bổng. Sau này làm BA hay PM, lương chục nghìn đô nha'
+    },
+    {
+        surname: 'Mai Lệ',
+        name: 'Quyên',
+        img: '../img/MaiQuyen.jpg',
+        desc:'Chúc Quyên 20/10 zui dẻ, xinh đẹp hơn, học giỏi hơn và có học bổng nha <3'
+    },
+    {
+        surname: 'Nguyễn Thu',
+        name: 'Quyên',
+        img: '../img/ThuQuyen.jpg',
+        desc:'Chúc Quyên 20-10 hạnh phúc bên cạnh những người mà cậu yêu quý <3!!!'
+    },
+    {
+        surname: 'Lê Minh',
+        name: 'Tân',
+        img: '../img/tan.jpg',
+        desc:'Chúc Tân sau này lm sếp Vietel, càng ngày càng giỏi và càng ngày càng giàu nhé:3 Best luck for you <3'
+    },
+    {
+        surname: 'Bùi Thị',
+        name: 'Thơm',
+        img: '../img/Thom.png',
+        desc:'Happy Vietnamese Women’s day, chúc Thơm có một ngày 20-10 zui zẻ, đạt được mọi mục tiêu đề ra để sớm làm chủ tịch và luôn đáng yêu như bây giờ nhé. From: Hội đàn em của Quang'
+    },
+    {
+        surname: 'Vũ Thị',
+        name: 'Huyền Trang',
+        img: '../img/trang.jpg',
+        desc:'Chào Trang. Chúc Trang luôn luôn vui tươi, xinh đẹp và thực hiện được những ước mơ, hoài bão của mình nhé'
+    },
+    
+];
+let picture = document.querySelectorAll('.effect-sarah');
+picture.forEach((element, index) => {
+    element.lastElementChild.firstElementChild.prepend(girl[index].surname + ' ');
+    element.lastElementChild.firstElementChild.firstElementChild.innerText = girl[index].name;
+    element.firstElementChild.src = girl[index].img;
+    element.addEventListener('click', () => {
+        Swal.fire({
+            title: `<h2 class="name">${girl[index].surname} ${girl[index].name}<h2>`,
+            html: 
+            `<p class="desc">${girl[index].desc}</p>`
+            ,
+            showCloseButton: true,
+            showCancelButton: false,
+            showDenyButton: true,
+            focusConfirm: false,
+            confirmButtonText: '<i class="fas fa-heart"></i> Có Yêu Đạt ',
+            confirmButtonAriaLabel: 'Thumbs up, great!',
+            ButtonText: '<i class="fa fa-thumbs-down"></i> Yêu Nam k?',
+            cancelButtonAriaLabel: 'Thumbs down',
+            padding: '1em',
+            background: '#fff url(/images/trees.png)',
+            backdrop: `
+                rgba(0,0,123,0.4)
+                url("https://sweetalert2.github.io/images/nyan-cat.gif")
+                left top
+                no-repeat
+            `,
+            denyButtonText: `<i class="fas fa-heart-broken"></i> Không Yêu Đạt`,
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: 'Đạt cũng yêu bạn <3',
+                    text: 'Yêu Đạt rồi thì yêu luôn hai bạn bên cạnh nhé :3',
+                    imageUrl: '../img/codat.jpg',
+                    imageWidth: 400,
+                    imageHeight: 400,
+                    imageAlt: 'Custom image',
+                })
+            } else if (result.isDenied) {
+                Swal.fire({
+                    title: 'Yêu Đạt đi!',
+                    text: 'Xin đấy :(',
+                    imageUrl: '../img/kodat.jpg',
+                    imageWidth: 400,
+                    imageHeight: 400,
+                    imageAlt: 'Custom image',
+                })
+            }
+        })
+    })
+})
